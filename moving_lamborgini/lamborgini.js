277f7e49962918d17a9efd1a40d6754ccfbece6f
your name <punyashloknath@gmail.com>
2025-06-16 00:06:27 +0530

    moving lamborgini is added

---
+++ moving_lamborgini/lamborgini.js
@@ -0,0 +1,12 @@
+// add sound of the car 
+
+var audio=document.createElement('audio');
+
+audio.setAttribute(
+  "src",
+  "assets/sounds-of-nuerburgring-engines-of-classic-race-cars-234929.mp3"
+);
+audio.loop=true;
+audio.play();
+
+
